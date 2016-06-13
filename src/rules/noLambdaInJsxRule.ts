abe08207c00cd4127cd758c22bd0e2c78b64e0ae
Adi Dahiya <adi.dahiya14@gmail.com>
2016-06-13 01:10:34 -0400

    Implement no-lambda-in-jsx rule

--- src/rules/noLambdaInJsxRule.ts
+++ src/rules/noLambdaInJsxRule.ts
@@ -2,6 +2,8 @@ import * as ts from "typescript";
 import * as Lint from "tslint/lib/lint";
 
 export class Rule extends Lint.Rules.AbstractRule {
+    public static FAILURE_STRING = "Lambdas are forbidden in JSX due to their rendering performance impact.";
+
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
         const noLambdaInJsxWalker = new NoLambdaInJsxWalker(sourceFile, this.getOptions());
         return this.applyWithWalker(noLambdaInJsxWalker);
@@ -9,7 +11,29 @@ export class Rule extends Lint.Rules.AbstractRule {
 }
 
 class NoLambdaInJsxWalker extends Lint.RuleWalker {
+    private isInJsxExpression = false;
+
     public visitJsxExpression(node: ts.JsxExpression) {
-        // TODO
+        this.isInJsxExpression = true;
+        super.visitJsxExpression(node);
+        this.isInJsxExpression = false;
+    }
+
+    public visitFunctionExpression(node: ts.FunctionExpression) {
+        if (this.isInJsxExpression) {
+            this.reportFailure(node);
+        }
+        super.visitFunctionExpression(node);
+    }
+
+    public visitArrowFunction(node: ts.ArrowFunction) {
+        if (this.isInJsxExpression) {
+            this.reportFailure(node);
+        }
+        super.visitArrowFunction(node);
+    }
+
+    private reportFailure(node: ts.Node) {
+        this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
     }
 }
