e770124dad81f5d5e808b30122eb21cd1a5d9d1d
David Lipowicz <dlipowicz@palantir.com>
2016-06-22 14:51:59 -0400

    Added new rule: jsx-no-multiline-js

---
+++ src/rules/jsxNoMultilineJsRule.ts
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Copyright 2016 Palantir Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as ts from "typescript";
+import * as Lint from "tslint/lib/lint";
+
+export class Rule extends Lint.Rules.AbstractRule {
+    public static FAILURE_STRING = "JSX expressions should not be multiline";
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        const walker = new JsxNoMultilineJsWalker(sourceFile, this.getOptions());
+        return this.applyWithWalker(walker);
+    }
+}
+
+class JsxNoMultilineJsWalker extends Lint.RuleWalker {
+    protected visitJsxExpression(node: ts.JsxExpression) {
+        if (node.getText().indexOf("\n") > -1) {
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+        }
+        super.visitJsxExpression(node);
+    }
+}
