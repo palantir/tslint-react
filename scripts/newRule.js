376bb7d05509aa6a9b0ad7f08461bfa7cefb8563
Dan Homola <homoldan@fit.cvut.cz>
2017-01-08 18:28:54 +0100

    Fix newRule.js lint issues

--- scripts/newRule.js
+++ scripts/newRule.js
@@ -32,8 +32,8 @@ const ruleTemplate =
  * limitations under the License.
  */
 
-import * as ts from "typescript";
 import * as Lint from "tslint";
+import * as ts from "typescript";
 
 export class Rule extends Lint.Rules.AbstractRule {
     public static FAILURE_STRING = "** ERROR MESSAGE HERE **";
