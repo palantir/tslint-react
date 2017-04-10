feea723c18731c411e741718ca4c4e6e65d40a62
Andrii Dieiev <illusionmh@gmail.com>
2017-04-10 23:18:06 +0300

    Always use TSLint from node_modules (fixes #82)

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -2,7 +2,7 @@
 
 ret=0;
 for path in `find test -name tslint.json`; do
-    tslint -r ./build/rules/ --test $path/..
+    ./node_modules/.bin/tslint -r ./build/rules/ --test $path/..
     val=$?
     if [ "$val" -ne "0" ]; then
         ret=$val
