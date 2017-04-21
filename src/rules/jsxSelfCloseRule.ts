80f6357a6d92da6fc9afe55e2e45034d39f9b3ae
Adi Dahiya <adi.dahiya14@gmail.com>
2017-04-21 14:11:31 -0400

    Refactor jsxSelfCloseRule to use walker function

--- src/rules/jsxSelfCloseRule.ts
+++ src/rules/jsxSelfCloseRule.ts
@@ -16,30 +16,32 @@
  */
 
 import * as Lint from "tslint";
+import { isJsxElement } from "tsutils";
 import * as ts from "typescript";
 
 export class Rule extends Lint.Rules.AbstractRule {
     public static FAILURE_STRING = "JSX elements with no children must be self-closing";
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        const walker = new JsxSelfCloseWalker(sourceFile, this.getOptions());
-        return this.applyWithWalker(walker);
+        return this.applyWithFunction(sourceFile, walk);
     }
 }
 
-class JsxSelfCloseWalker extends Lint.RuleWalker {
-    protected visitJsxElement(node: ts.JsxElement) {
-        const missingOpeningOrClosingTag = node.openingElement == null || node.closingElement == null;
-        // The last part of the textIsEmpty assignment is to check whether the tag is completely empty or
-        // only consists of spaces/new lines.
-        const textIsEmpty = node.children.length === 1
-            && node.children[0].kind === ts.SyntaxKind.JsxText
-            && node.children[0].getText() === "";
-        const noChildren = node.children.length === 0 || textIsEmpty;
+function walk(ctx: Lint.WalkContext<void>): void {
+    return ts.forEachChild(ctx.sourceFile, function cb(node: ts.Node): void {
+        if (isJsxElement(node)) {
+            const missingOpeningOrClosingTag = node.openingElement == null || node.closingElement == null;
+            // The last part of the textIsEmpty assignment is to check whether the tag is completely empty or
+            // only consists of spaces/new lines.
+            const textIsEmpty = node.children.length === 1
+                && node.children[0].kind === ts.SyntaxKind.JsxText
+                && node.children[0].getText() === "";
+            const noChildren = node.children.length === 0 || textIsEmpty;
 
-        if (missingOpeningOrClosingTag || noChildren) {
-            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+            if (missingOpeningOrClosingTag || noChildren) {
+                ctx.addFailureAtNode(node, Rule.FAILURE_STRING);
+            }
         }
-        super.visitJsxElement(node);
-    }
+        return ts.forEachChild(node, cb);
+    });
 }
